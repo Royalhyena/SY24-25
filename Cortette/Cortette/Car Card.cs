18a60ac5aafc80f79b33fcec3e3795fda63c6c0f
Royal <Royalhyena8@gmail.com>
2025-02-12 19:10:38 +0300

    Cortetteification

---
+++ Cortette/Cortette/Car Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cortette
+{
+    internal class Car_Card
+    {
+        public int Maxspeed { get; set; }
+        public double ZeroTwo60 { get; set; }
+        public int HorsePower { get; set; }
+        public int CC { get; set; }
+        public int Cylinders { get; set; }
+        public int RPM { get; set; }
+    }
+}
